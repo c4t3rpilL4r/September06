5639e676743f73df76a442fd5692d123c7188ba0
Rap <Rap@DESKTOP-6ALDOJE>
2019-09-06 17:14:28 +0800

    First_Exercise is now working

--- First_Exercise/Program.cs
+++ First_Exercise/Program.cs
@@ -9,11 +9,7 @@ namespace First_Exercise
             Console.Write("Please enter word: ");
 
             string input = Console.ReadLine();
-            int middle = input.Length / 2;
-            Console.WriteLine(input.Length % 2 == 1 ? $"Output is: {input[middle]}" : $"Output is: {input[middle - 1]}{input[middle]}");
-            Console.WriteLine(input.Length % 2 == 1 ? $"Output is: {input[input.Length / 2]}" : $"Output is: {input[(input.Length / 2) - 1]}{input[input.Length / 2]}");
-            Console.WriteLine($"Output is: {(input.Length % 2 == 1 ? input[middle] : input[middle - 1] + input[middle])}");
-            Console.WriteLine($"Output is: {(input.Length % 2 == 1 ? input[middle] : input.Substring(middle - 1, 2))}");
+            Console.WriteLine($"Output is: {(input.Length % 2 == 1 ? input[input.Length / 2].ToString() : input.Substring((input.Length / 2) - 1, 2))}");
             Console.ReadLine();
         }
     }
